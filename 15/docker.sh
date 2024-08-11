721672e79f7ccb3b7e44f39718ea868d7b2f2bf3
Pavel Lebedev <pav.leb.art@yandex.ru>
2024-08-11 20:16:53 +0300

    docker compose

--- 15/docker.sh
+++ 15/docker.sh
@@ -6,3 +6,25 @@ docker run --rm -d \
     --net=app \
     -name db \
     postgres
+
+docker run --rm -d 
+    --name postgres_db \
+    --net=db_net \
+    -e POSTGRES_USER= \
+    -e POSTGRES_PASSWORD= \
+    -e POSTGRES_DB= postgres:14
+
+docker run --rm -d \
+    --name backend \
+    --net=db_net \
+    -e PG_HOST=postgres_db \
+    -e PG_USER= \
+    -e PG_PASSWORD= \
+    -e PG_DATABASE= \
+    kcoursedocker/task-7.4-back
+
+docker run --rm -d \
+    --name frontend \
+    --net=db_net \
+    -p 80:80 \
+    kcoursedocker/task-7.4-front
